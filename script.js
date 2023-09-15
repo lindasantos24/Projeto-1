078fccf68975efe829da2fd9df3e93e4a23a3290
Linda Karolayne <linda_karolayne@hotmail.com>
2023-09-15 10:56:31 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+
+const email=document.getElementById("email");
+const password=document.getElementById("password");
+
+if( email== "" || password == "") {
+  alert("Os campos usuário e senha são obrigatórios");
+} else {
+    alert("Funcionou");
+}
